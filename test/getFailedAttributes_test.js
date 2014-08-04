42cb466792a5366cf1bba2d2e9f1dc0938e5b4a0
Carlos A. Guillen <caguillen214@gmail.com>
2014-08-04 16:12:38 -0700

    feat(*): implement detection of incorrect ngRepeat usage

--- test/getFailedAttributes_test.js
+++ test/getFailedAttributes_test.js
@@ -27,6 +27,24 @@ describe('getFailedAttributes()', function() {
     expect(results.length).toBe(0);
   });
 
+  it('should identify if ngRepeat does not match "track by PROP_NAME | filter:VAR_NAME" structure', function() {
+    var elementToTest = [{nodeName: 'ng-repeat', value: 'i in items | filter:search track by id'}];
+    var results = getFailedAttributes(elementToTest, options);
+    expect(results.length).toBe(0);
+
+    elementToTest = [{nodeName: 'ng-repeat', value: 'i in items filter:search'}];
+    results = getFailedAttributes(elementToTest, options);
+    expect(results[0].suggestion).toBe('Try: " | filter:search"');
+
+    elementToTest = [{nodeName: 'ng-repeat', value: 'i in items | track by id filter:search'}];
+    results = getFailedAttributes(elementToTest, options);
+    expect(results[0].suggestion).toBe('Try: " | filter:search track by id"');
+
+    elementToTest = [{nodeName: 'ng-repeat', value: 'i in items filter:search track by id'}];
+    results = getFailedAttributes(elementToTest, options);
+    expect(results[0].suggestion).toBe('Try: " | filter:search track by id"');
+  });
+
   it('should identify html event attributes', function() {
      var elementToTest = [{nodeName: 'onclick'}];
      var results = getFailedAttributes(elementToTest,options);
