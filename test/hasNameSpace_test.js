e4684a6a4426c5721646ff7177f57ad4f1ffb3b6
Carlos A. Guillen <caguillen214@gmail.com>
2014-08-05 10:33:32 -0700

    feat(hasNameSpace): improve nameSpace validation, closes #26 and #27

--- test/hasNameSpace_test.js
+++ test/hasNameSpace_test.js
@@ -3,5 +3,7 @@ var hasNameSpace = require('../lib/hasNameSpace');
 describe('hasNameSpace', function() {
   it('should identify missing name spaces', function(){
     expect(hasNameSpace('nonamespace')).toBe(false);
+    expect(hasNameSpace('yesNameSpace')).toBe(true);
+    expect(hasNameSpace('ngNameSpace')).toBe(false);
   });
 });
